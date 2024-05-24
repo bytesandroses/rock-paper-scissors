663e6c1d5e8e36071db33f5b93ea39400a3b4cbc
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-05-24 10:18:02 +0200

    Add stopGame() function to disable buttons when either the user or computer has won 5 rounds

--- script.js
+++ script.js
@@ -65,31 +65,18 @@ function playRound(humanChoice, computerChoice) {
           break;
       }
     }
+
+    stopGame(); 
   }
 
   humanScoreDiv.innerHTML = (`<strong>Your Score:</strong> ${humanScore}`);
   computerScoreDiv.innerHTML = (`<strong>Computer Score:</strong> ${computerScore}`);
 }
 
-function playGame() {
-  let humanChoice;
-  let computerChoice;
-
-/*  for (let round = 0; round < 5; round++) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
+function stopGame() {
+  if (humanScore === 5 || computerScore === 5) {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
   }
-
-  if (humanScore > computerScore) {
-    console.log("Great job! You've won the game!");
-  }
-  else if (humanScore < computerScore) {
-    console.log("Sorry, you lose!");
-  }
-  else if (humanScore == computerScore) {
-    console.log("You tied!");
-  } */
 }
-
-// playGame(); 
